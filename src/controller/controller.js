6858b43a16a80dd89aedcd1a55c7209ca5daae39
Subham Bose <subhambose@yahoo.in>
2021-11-12 02:07:13 +0530

    third

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -8,18 +8,18 @@ const addtion = (req, res) => {
 
     if (typeof num1 != "number" || typeof num2 != "number") {
       return res
-        .status(400)
+        .status(200)
         .json({ status: "error", message: "Invalid data types" });
     } else if (num1 < -1000000 || num2 < -1000000) {
-      return res.status(400).json({ status: "error", message: "Underflow" });
+      return res.status(200).json({ status: "error", message: "Underflow" });
     } else if (num1 > 1000000 || num2 > 1000000) {
-      return res.status(400).json({ status: "error", message: "Overflow" });
+      return res.status(200).json({ status: "error", message: "Overflow" });
     } else {
       const result = num1 + num2;
       if (result > 1000000)
-        return res.status(400).json({ status: "error", message: "Overflow" });
+        return res.status(200).json({ status: "error", message: "Overflow" });
       else if (result < -1000000)
-        return res.status(400).json({ status: "error", message: "Underflow" });
+        return res.status(200).json({ status: "error", message: "Underflow" });
       res.status(200).json({
         status: "success",
         message: "the sum of given two numbers",
@@ -36,18 +36,18 @@ const substraction = (req, res) => {
 
     if (typeof num1 != "number" || typeof num2 != "number") {
       return res
-        .status(400)
+        .status(200)
         .json({ status: "error", message: "Invalid data types" });
     } else if (num1 < -1000000 || num2 < -1000000) {
-      return res.status(400).json({ status: "error", message: "Underflow" });
+      return res.status(200).json({ status: "error", message: "Underflow" });
     } else if (num1 > 1000000 || num2 > 1000000) {
-      return res.status(400).json({ status: "error", message: "Overflow" });
+      return res.status(200).json({ status: "error", message: "Overflow" });
     } else {
       const result = num1 - num2;
       if (result > 1000000)
-        return res.status(400).json({ status: "error", message: "Overflow" });
+        return res.status(200).json({ status: "error", message: "Overflow" });
       else if (result < -1000000)
-        return res.status(400).json({ status: "error", message: "Underflow" });
+        return res.status(200).json({ status: "error", message: "Underflow" });
       res.status(200).json({
         status: "success",
         message: "the difference of given two numbers",
@@ -64,18 +64,18 @@ const multiplication = (req, res) => {
 
     if (typeof num1 != "number" || typeof num2 != "number") {
       return res
-        .status(400)
+        .status(200)
         .json({ status: "error", message: "Invalid data types" });
     } else if (num1 < -1000000 || num2 < -1000000) {
-      return res.status(400).json({ status: "error", message: "Underflow" });
+      return res.status(200).json({ status: "error", message: "Underflow" });
     } else if (num1 > 1000000 || num2 > 1000000) {
-      return res.status(400).json({ status: "error", message: "Overflow" });
+      return res.status(200).json({ status: "error", message: "Overflow" });
     } else {
       const result = num1 * num2;
       if (result > 1000000)
-        return res.status(400).json({ status: "error", message: "Overflow" });
+        return res.status(200).json({ status: "error", message: "Overflow" });
       else if (result < -1000000)
-        return res.status(400).json({ status: "error", message: "Underflow" });
+        return res.status(200).json({ status: "error", message: "Underflow" });
       res.status(200).json({
         status: "success",
         message: "The product of given numbers",
@@ -92,22 +92,22 @@ const division = (req, res) => {
 
     if (typeof num1 != "number" || typeof num2 != "number") {
       return res
-        .status(400)
+        .status(200)
         .json({ status: "error", message: "Invalid data types" });
     } else if (num1 < -1000000 || num2 < -1000000) {
-      return res.status(400).json({ status: "error", message: "Underflow" });
+      return res.status(200).json({ status: "error", message: "Underflow" });
     } else if (num1 > 1000000 || num2 > 1000000) {
-      return res.status(400).json({ status: "error", message: "Overflow" });
+      return res.status(200).json({ status: "error", message: "Overflow" });
     } else if (num2 === 0) {
       return res
-        .status(400)
+        .status(200)
         .json({ status: "error", message: "Cannot divide by zero" });
     } else {
       const result = num1 + num2;
       if (result > 1000000)
-        return res.status(400).json({ status: "error", message: "Overflow" });
+        return res.status(200).json({ status: "error", message: "Overflow" });
       else if (result < -1000000)
-        return res.status(400).json({ status: "error", message: "Underflow" });
+        return res.status(200).json({ status: "error", message: "Underflow" });
       res.status(200).json({
         status: "success",
         message: "The division of given numbers",
